2ae997e8867482d3a8375d2ec49d923718602bb0
bares12 <40828949+bares12@users.noreply.github.com>
2018-07-13 01:19:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -69,7 +69,7 @@ function shalat($keyword) {
 
 //show menu, saat join dan command /menu
 if ($type == 'join' || $command == '/menu') {
-    $text = "Assalamualaikum Kakak, aku adalah bot jadwal shalat, silahkan ketik\n\n/shalat <nama tempat>\n\nnanti aku bakalan kasih tahu jam berapa waktunya shalat ^_^";
+    $text = "Assalamualaikum Kakak, aku adalah bot, silahkan ketik\n\n/help \n\nUntuk Melihat Menu";
     $balas = array(
         'replyToken' => $replyToken,
         'messages' => array(
@@ -104,7 +104,7 @@ if($message['type']=='text') {
 							'messages' => array(
 								array(
 										'type' => 'text',									
-										'text' => 'Makasih Kak Stikernya ^_^'										
+										'text' => 'Itu Sticker Apa Ka'										
 									
 									)
 							)
